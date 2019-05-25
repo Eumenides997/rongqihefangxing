ec169326acb5657d92e9de0f3bba34055a9c51b1
Eumenides997 <646224348@qq.com>
2019-05-25 16:06:04 +0800

    第八次上传

---
+++ src/容器与泛型作业_3/test_3.java
@@ -0,0 +1,18 @@
+package 容器与泛型作业_3;
+
+import java.util.HashMap;
+
+public class test_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		候选人 s1=new 候选人("zhangsan","c1");
+		候选人 s2=new 候选人("lisi","c2");
+		候选人 s3=new 候选人("wanwu","c3");
+		
+		HashMap<候选人,String> m=new HashMap<候选人,String>();
+
+	}
+
+}
---
+++ src/容器与泛型作业_3/候选人.java
@@ -0,0 +1,21 @@
+package 容器与泛型作业_3;
+
+public class 候选人 {
+	
+	private String name;
+	private String no;
+	
+	public 候选人(String name,String no) {
+		this.name=name;
+		this.no=no;
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public String getno() {
+		return no;
+	}
+
+}
