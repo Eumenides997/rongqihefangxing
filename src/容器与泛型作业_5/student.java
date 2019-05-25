788c7b14421a170d5f26efa903b25523ad816723
Eumenides997 <646224348@qq.com>
2019-05-25 18:52:24 +0800

    第十六次上传

--- src/容器与泛型作业_5/student.java
+++ src/容器与泛型作业_5/student.java
@@ -1,11 +1,11 @@
 package 容器与泛型作业_5;
 
-public class student {
-	
+
+public class student implements Comparable<student>{
 
 	private String Sno;
 	private String Sname;
-	private int Sacount;
+	int Sacount;
 	
 	public student(String Sno,String Sname,int Sacount){
 		super();
@@ -36,6 +36,21 @@ public class student {
 	
 	public int getSacount() {
 		return Sacount;
+	}
+	/*方法重写(从大到小排序)*/
+	@Override
+	public int compareTo(student o) {
+		// TODO Auto-generated method stub
+		student p = (student)o;  
+	      if(p.Sacount>this.Sacount){  
+	     return 1;  
+	      }  
+	      else if(p.Sacount<this.Sacount){  
+	     return -1;  
+	      }  
+	      else{  
+	          return 0;  
+	      }  
 	}
 
 
