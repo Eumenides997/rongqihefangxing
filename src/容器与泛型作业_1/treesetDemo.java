0ddbf5a2572158218766fd433aa6908a2d5888a5
Eumenides997 <646224348@qq.com>
2019-05-25 15:05:48 +0800

    第四次上传

--- src/容器与泛型作业_1/treesetDemo.java
+++ src/容器与泛型作业_1/treesetDemo.java
@@ -13,11 +13,17 @@ public class treesetDemo {
 		ts.add(new student("c1","zhangsan",100));
 		ts.add(new student("c2","lisi",100));
 		ts.add(new student("c3","wanwu",100));
-		
+		ts.add(new student("c4","niuliu",99));
+		/*
 		for(student s:ts) {
 			System.out.println(s.getSno()+"-"+s.getSname()+"-"+s.getSacount());
 		}
-		
+		*/
+		Iterator it=ts.iterator();
+		while(it.hasNext()) {
+			student st=(student)it.next();
+			System.out.println(st.getSname()+" "+st.getSno()+" "+st.getSacount());
+		}
 
 	}
 
