6d71d5dfad9531f04186d66333579ae377e44a2c
Eumenides997 <646224348@qq.com>
2019-05-25 15:41:47 +0800

    第五次上传

---
+++ src/容器与泛型作业_2/student.java
@@ -0,0 +1,24 @@
+package 容器与泛型作业_2;
+
+public class student {
+	
+	private String name;
+	private int age;
+	
+	private void setname(String name) {
+		this.name=name;
+	}
+	
+	private String getname() {
+		return name;
+	}
+	
+	private void setage(int age) {
+		this.age=age;
+	}
+	
+	private int getage() {
+		return age;
+	}
+
+}
